874351064eb2d5933609a645bcb19635266e8ac7
Jun <changechunjunhao@gmail.com>
2018-01-21 23:00:56 +0800

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,15 @@
+export LANG=en_US.utf-8
+git config --global user.email changchunjunhao@gmail.com
+git config --global user.name zhengjunhao
+
+alias emacs="emacs --color=no"
+cat >> ~/.emacs <<EOF
+(setq backup-directory-alist \`(("." . "~/.saves")))
+(put 'upcase-region 'disabled nil)
+
+(setq c-default-style "linux"
+     c-basic-offset 4)
+
+EOF
+
+
