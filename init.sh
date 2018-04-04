afc60a6da33b344c4ca83127285b868be1d8d4ae
zhengjunhao <changchunjunhao@gmail.com>
2018-04-04 19:29:14 +0800

    Add docker installation

--- init.sh
+++ init.sh
@@ -3,6 +3,10 @@ git config --global user.email changchunjunhao@gmail.com
 git config --global user.name zhengjunhao
 git config --global core.editor emacs
 
+sudo apt-get update
+
+# emacs
+echo Y | sudo apt-get install emacs
 alias emacs="emacs --color=no"
 cat >> ~/.emacs <<EOF
 (setq backup-directory-alist \`(("." . "~/.saves")))
@@ -13,4 +17,31 @@ cat >> ~/.emacs <<EOF
 
 EOF
 
+# hosts
+# [ -e "/etc/hosts.bak" ] || sudo cp /etc/hosts /etc/hosts.bak
+# cd /tmp
+# git clone https://github.com/googlehosts/hosts.git
+# f=`find hosts/ -name hosts`
+# [ -e "$f" ] && {
+#     cat $f >> /etc/hosts
+# }
+# cd -
 
+# install others
+echo Y | sudo apt-get install python3 tmux meld
+
+# set capslock as ctrl
+sudo sed -e "/XKBOPTIONS/s/.*/XKBOPTIONS=\"ctrl:nocaps\"/" -i /etc/default/keyboard
+
+# install docker
+echo Y | sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+echo Y | sudo apt-get install docker-ce
+sudo groupadd docker
+sudo usermod -aG docker $USER
